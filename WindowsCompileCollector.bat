82fc6a2902b443f2c9a40aba97f1dc6d3552d54f
anthonyhaffey <anthony.haffey@gmail.com>
2020-03-30 18:32:55 +0100

    Compiling exe should be a bit smoother
    Helper should be fixed

--- WindowsCompileCollector.bat
+++ WindowsCompileCollector.bat
@@ -1,5 +1,4 @@
-#venv\Scripts\Activate.bat
-python -m eel Collector.py web --onefile --noconsole
+python -m eel Collector.py web --onefile --noconsole --icon=collector.ico
 copy dist\Collector.exe Collector.exe
 @RD /S /Q build
 @RD /S /Q dist
